82025b5acae1ea7d5798be12db8aa7b2617dcc0d
vilela1966 <airton.vilela@gmail.com>
2017-02-14 20:41:54 -0200

    primeira página usando Blade

---
+++ resources/views/ola.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Olá</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css" />
+  </head>
+  <body>
+    <div class="container">
+      @for ($i=0; $i < 10; $i++)
+          <h2>Boa noite</h2>
+      @endfor
+    </div>
+    <script type="text/javascript" src="/js/app.js">
+  </body>
+</html>
